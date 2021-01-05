5e09277261cb66dd473f93ba3c78a2694525600f
Артемий <va2005art@gmail.com>
2021-01-06 01:09:38 +0300

    add category views and search edittext

--- app/src/main/java/com/example/onlinestoretechniques/MainActivity.kt
+++ app/src/main/java/com/example/onlinestoretechniques/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.onlinestoretechniques
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        lin_main.requestFocus()
     }
 }
