aaed90b97f9b91f1a7fff2b933dc4ac078a4fc1a
Артемий <va2005art@gmail.com>
2021-01-06 22:06:45 +0300

    update choose category

---
+++ app/src/main/java/com/example/onlinestoretechniques/FragmentMain.kt
@@ -0,0 +1,74 @@
+package com.example.onlinestoretechniques
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.lin_category.*
+
+class FragmentMain: Fragment(){
+    var list_hot = DataProducts().listPhones.toMutableList()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        rec_hot.apply{
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        }
+        updateAdapterRec()
+        setFragment(FragmentRecommendation())
+
+        initClickCategory()
+    }
+
+    private fun initClickCategory(){
+        card_main.setOnClickListener {
+            setFragment(FragmentRecommendation())
+            card_main.visibility = View.GONE
+        }
+
+        card_watch.setOnClickListener {setFragment(FragmentCategory("Watch", DataProducts().listWatch))
+            card_main.visibility = View.VISIBLE}
+        card_cameras.setOnClickListener {setFragment(FragmentCategory("Cameras", DataProducts().listCamera))
+            card_main.visibility = View.VISIBLE}
+        card_head.setOnClickListener {setFragment(FragmentCategory("Headphones", DataProducts().listHead))
+            card_main.visibility = View.VISIBLE}
+        card_keyboards.setOnClickListener {setFragment(FragmentCategory("Keyboards", DataProducts().listKey))
+            card_main.visibility = View.VISIBLE}
+        card_printers.setOnClickListener{setFragment(FragmentCategory("Printers", DataProducts().listPrint))
+            card_main.visibility = View.VISIBLE}
+        card_phones.setOnClickListener{setFragment(FragmentCategory("Phones", DataProducts().listPhones))
+            card_main.visibility = View.VISIBLE}
+        card_laptop.setOnClickListener{setFragment(FragmentCategory("Laptops", DataProducts().listLaptops))
+            card_main.visibility = View.VISIBLE}
+        card_mice.setOnClickListener{setFragment(FragmentCategory("Mice", DataProducts().listMice))
+            card_main.visibility = View.VISIBLE}
+        card_tv.setOnClickListener{setFragment(FragmentCategory("Tv", DataProducts().listTv))
+            card_main.visibility = View.VISIBLE}
+        card_tablets.setOnClickListener{setFragment(FragmentCategory("Tablets", DataProducts().listTablets))
+            card_main.visibility = View.VISIBLE}
+    }
+
+    private fun updateAdapterRec(){
+        rec_hot.adapter = AdapterProduct(list_hot, R.layout.item_hit)
+    }
+
+    private fun setFragment(fragment: Fragment){
+        val fr = childFragmentManager
+        val tr = fr.beginTransaction()
+        if (fr.fragments.size != 0) {
+            tr.remove(fr.fragments[0])
+        }
+        tr.add(R.id.frame, fragment)
+        tr.commit()
+    }
+}
--- app/src/main/java/com/example/onlinestoretechniques/MainActivity.kt
+++ app/src/main/java/com/example/onlinestoretechniques/MainActivity.kt
@@ -2,32 +2,15 @@ package com.example.onlinestoretechniques
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.onlinestoretechniques.model.ModelProduct
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.lin_category.*
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
-    var list_hot = DataProducts().listPhones.toMutableList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        lin_main.requestFocus()
-        rec_hot.apply{
-            layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
-        }
-        updateAdapterRec()
-        setFragment(FragmentRecommendation())
-    }
 
-    private fun updateAdapterRec(){
-        rec_hot.adapter = AdapterProduct(list_hot, R.layout.item_hit)
+        setFragment(FragmentMain())
     }
 
     private fun setFragment(fragment: Fragment){
@@ -36,24 +19,7 @@ class MainActivity : AppCompatActivity() {
         if (fr.fragments.size != 0) {
             tr.remove(fr.fragments[0])
         }
-        tr.add(R.id.frame, fragment)
+        tr.add(R.id.frame_main, fragment)
         tr.commit()
-    }
-
-    fun clickToCategory(v: View){
-        var text = "Category"
-        when (v.id){
-            R.id.card_phones -> text = "Phones"
-            R.id.card_cameras -> text = "Cameras"
-            R.id.card_laptop -> text = "Laptops"
-            R.id.card_head -> text = "Headphones"
-            R.id.card_tv -> text = "Tv"
-            R.id.card_keyboards -> text = "Keyboards"
-            R.id.card_tablets -> text = "Tablets"
-            R.id.card_mice -> text = "Mice"
-            R.id.card_printers -> text = "Printers"
-            R.id.card_watch -> text = "Watch"
-        }
-        setFragment(FragmentCategory(text, list_hot))
     }
 }
